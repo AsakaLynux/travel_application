252db21ec6da85032be4e744168c69dcb6f5e7ba
AsakaLynux <asakalynux@gmail.com>
2024-11-03 11:17:04 +0700

    Create Transaction detail page

---
+++ lib/ui/pages/transaction_detail_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TransactionDetailPage extends StatelessWidget {
+  const TransactionDetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Transaction detail page"),
+      ),
+    );
+  }
+}
