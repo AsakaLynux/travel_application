c79aa73d1aa8f3a045e7567df3b1aae56afd3817
AsakaLynux <asakalynux@gmail.com>
2024-11-16 17:16:03 +0700

    Create admin page

---
+++ lib/ui/admin/admin_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import '../../shared/theme.dart';
+
+class AdminPage extends StatelessWidget {
+  const AdminPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: Column(
+        children: [
+          Text("Admin Page"),
+        ],
+      ),
+    );
+  }
+}
