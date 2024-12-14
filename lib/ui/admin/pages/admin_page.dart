aed630f1152ca167c211a633cbcaadb380b4565d
AsakaLynux <asakalynux@gmail.com>
2024-12-14 14:35:59 +0700

    Center the body

--- lib/ui/admin/pages/admin_page.dart
+++ lib/ui/admin/pages/admin_page.dart
@@ -37,58 +37,62 @@ class AdminPage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: kBackgroundColor,
-      body: Column(
-        children: [
-          Text(
-            "Admin Page",
-            style: blackTextStyle.copyWith(fontSize: 16, fontWeight: semiBold),
-          ),
-          CustomButton(
-            text: "Destination",
-            width: 212,
-            onPressed: () {
-              Navigator.pushNamed(context, "/DestinationPage");
-            },
-          ),
-          CustomButton(
-            text: "Account",
-            width: 212,
-            onPressed: () {},
-          ),
-          CustomButton(
-            text: "Transaction",
-            width: 212,
-            onPressed: () {},
-          ),
-          CustomButton(
-            text: "Delete",
-            width: 122,
-            onPressed: () async {
-              dialog(
-                () async {
-                  final deleteUserInfo = await userServices.deleteUser();
-                  if (deleteUserInfo) {
-                    if (context.mounted) {
-                      Navigator.pushNamedAndRemoveUntil(
-                        context,
-                        "/",
-                        (Route<dynamic> route) => false,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Admin Page",
+              style:
+                  blackTextStyle.copyWith(fontSize: 16, fontWeight: semiBold),
+            ),
+            CustomButton(
+              text: "Destination",
+              width: 212,
+              onPressed: () {
+                Navigator.pushNamed(context, "/DestinationPage");
+              },
+            ),
+            CustomButton(
+              text: "Account",
+              width: 212,
+              onPressed: () {},
+            ),
+            CustomButton(
+              text: "Transaction",
+              width: 212,
+              onPressed: () {},
+            ),
+            CustomButton(
+              text: "Delete",
+              width: 122,
+              onPressed: () async {
+                dialog(
+                  () async {
+                    final deleteUserInfo = await userServices.deleteUser();
+                    if (deleteUserInfo) {
+                      if (context.mounted) {
+                        Navigator.pushNamedAndRemoveUntil(
+                          context,
+                          "/",
+                          (Route<dynamic> route) => false,
+                        );
+                      }
+                      return Fluttertoast.showToast(
+                        msg: "Success Delete Account",
+                        toastLength: Toast.LENGTH_SHORT,
+                        gravity: ToastGravity.BOTTOM,
+                        backgroundColor: kWhiteColor,
+                        textColor: kBlackColor,
+                        fontSize: 16.0,
                       );
                     }
-                    return Fluttertoast.showToast(
-                      msg: "Success Delete Account",
-                      toastLength: Toast.LENGTH_SHORT,
-                      gravity: ToastGravity.BOTTOM,
-                      backgroundColor: kWhiteColor,
-                      textColor: kBlackColor,
-                      fontSize: 16.0,
-                    );
-                  }
-                },
-              );
-            },
-          ),
-        ],
+                  },
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
